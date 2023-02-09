3408ff559bc27ff27fbd53f30f37e8ebcef75a72
mdekabs <1mawoda@gmail.com>
2023-02-09 09:08:18 +0100

    still working on it

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+	importing modules
+"""
+
+import datetime as DT 
+import uuid as UD 
+class Base_model:
+	"""
+		This is the basemodel where i have all the 
+		variables and method defined.
+	"""
+
+	def __init__(self, id, created_at, updated_at):
+		self.id = str(UD.uuid4()) 
+		self.created_at = DT.datetime.now()
+		self.updated_at = self.created_at 
+
+
+	def __str__(self):
+		"""
+			return should print: [<class name>] (<self.id>) <self.__dict__>
+		"""
+		return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+	def save(self):
+		"""
+			 updates the public instance attribute updated_at with the current datetime
+		"""
+		self.updated_at = DT.datetiem.now()
+
+	def to_dict(self):
+		"""
+			returns a dictionary containing all keys/values of __dict__ of the instance.
+		"""
+		 dictionary = self.__dict__
+
+
+
