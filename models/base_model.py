8fbe66c1da446596ecde12f0ea7ca76454d898ef
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-02-11 23:10:37 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -53,19 +53,3 @@ class BaseModel:
 		self.__dict__['updated_at'] = self.updated_at.isoformat()
 
 		return self.__dict__
-
-
-
-if __name__ == "__main__":
-	all_objs = storage.all()
-	print("-- Reloaded objects --")
-	for obj_id in all_objs.keys():
-		obj = all_objs[obj_id]
-		print(obj)
-
-	print("-- Create a new object --")
-	my_model = BaseModel()
-	my_model.name = "My_First_Model"
-	my_model.my_number = 89
-	my_model.save()
-	print(my_model)
