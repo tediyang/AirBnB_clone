2c37b542dddd122baadb3a665c6611d0275d5234
mdekabs <1mawoda@gmail.com>
2023-02-14 08:26:51 +0100

    committing

--- tests/test_amenity.py
+++ tests/test_amenity.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" testing amenity """
+
+import unittest
+from models.amenity import Amenity as AT
+from models.base_model import BaseModel as BM
+
+class test_AT(unittest.TestCase):
+	""" testing class """
+	
