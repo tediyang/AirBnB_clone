3d9058701ef150e3edf4f93ecb7e9d100500ce32
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-02-14 17:20:09 +0100

    changes made to city test files

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -18,8 +18,8 @@ class test_City(unittest.TestCase):
             setup.
         """
         cls.mock_city = City()
-        cls.mock_city.name = "test"
-        cls.mock_city.state_id = State().id
+        cls.mock_city.name = None
+        cls.mock_city.state_id = None
 
     @classmethod
     def tearDownClass(cls):
