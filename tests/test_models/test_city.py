ed48c664a14adf86dc67f7248a7923669c50af23
mdekabs <1mawoda@gmail.com>
2023-02-14 10:30:34 +0100

    adding all

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    test city
+"""
+from models.city import City
+from models.state import State
+from models.base_model import BaseModel
+import unittest
+
+
+class test_City(unittest.TestCase):
+    """
+        test for city class
+    """
+    @classmethod
+    def setUpClass(cls):
+        """
+            setup.
+        """
+        cls.mock_city = City()
+        cls.mock_city.name = "test"
+        cls.mock_city.state_id = State().id
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+            tear down
+        """
+        del cls.mock_city
+
+    def test_inheritance(self):
+        """
+            test proper inheritance
+        """
+        self.assertIsInstance(self.mock_city, BaseModel)
+        self.assertTrue(hasattr(self.mock_city, "id"))
+        self.assertTrue(hasattr(self.mock_city, "created_at"))
+        self.assertTrue(hasattr(self.mock_city, "updated_at"))
+
+    def test_attrs(self):
+        """
+            test attributes
+        """
+        self.assertTrue(hasattr(self.mock_city, "name"))
+        self.assertTrue(hasattr(self.mock_city, "state_id"))
+
+if __name__ == "__main__":
+    unittest.main()
