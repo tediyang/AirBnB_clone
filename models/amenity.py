db13f7143ed61b26d04789ff78a892b8d8b5f090
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-29 02:33:19 +0100

    update city, place, user and review object

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+""" Amenity Module for AirBnB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-""" importing module """
 
-from models.base_model import BaseModel
-
-class Amenity(BaseModel):
-	""" class with pub attr """
-	name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity object: Inherits from BaseModel and Base (sqlalchemy) """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                         viewonly=False,
+                         backref="amenities")
