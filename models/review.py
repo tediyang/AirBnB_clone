9fdcf99a09f603025e2f52b6cc812b3f082305e7
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-29 01:14:27 +0100

    update city, place, user and review object

--- models/review.py
+++ models/review.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+""" Review module for the AirBnB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
-""" importing neccessary modules """
 
-from models.base_model import BaseModel
-
-class Review(BaseModel):
-	""" class attr """
-	place_id = ""
-	user_id = ""
-	text = ""
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
